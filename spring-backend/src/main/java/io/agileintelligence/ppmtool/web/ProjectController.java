4caa3ffa995bb85e34b2f745f320b2f12bef2eb6
Avinandan Banerjee <avinandanbanerjee99@gmail.com>
2020-11-04 12:10:49 +0530

    Enabled CORS

--- spring-backend/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ spring-backend/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -12,6 +12,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/project")
+@CrossOrigin
 public class ProjectController {
 
     private final ProjectService projectService;
